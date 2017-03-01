58c2c129ca8046d9d00d2d5dbf6b9537ef78b0b9
Zhou Yu <yuzhou@oymotion.com>
2017-03-01 18:05:51 +0800

    Added WinHttpWebSocket client

--- libSDK/App/RawDataCapture/WinHttpWebSocket.cpp
+++ libSDK/App/RawDataCapture/WinHttpWebSocket.cpp
@@ -5,248 +5,223 @@
 //
 // Copyright (c) Microsoft Corporation. All rights reserved
 
+#include "stdafx.h"
 #include <Windows.h>
 #include <WinHttp.h>
 #include <stdio.h>
 
-int __cdecl wmain()
+#include "WinHttpWebSocket.h"
+
+const WCHAR * const WinHttpWebSocket::m_userAgent = L"OYMotion WebSocket";
+const WCHAR * const WinHttpWebSocket::m_path = L"/RawData";
+
+
+int WinHttpWebSocket::Open(const WCHAR *server, INTERNET_PORT port)
 {
-    DWORD dwError = ERROR_SUCCESS;
-    BOOL fStatus = FALSE;
-    HINTERNET hSessionHandle = NULL;
-    HINTERNET hConnectionHandle = NULL;
-    HINTERNET hRequestHandle = NULL;
-    HINTERNET hWebSocketHandle = NULL;
-    BYTE rgbCloseReasonBuffer[123];
-    BYTE rgbBuffer[1024];
-    BYTE *pbCurrentBufferPointer = rgbBuffer;
-    DWORD dwBufferLength = ARRAYSIZE(rgbBuffer);
-    DWORD dwBytesTransferred = 0;
-    DWORD dwCloseReasonLength = 0;
-    USHORT usStatus = 0;
-    WINHTTP_WEB_SOCKET_BUFFER_TYPE eBufferType;
-    INTERNET_PORT Port = 8888;// INTERNET_DEFAULT_HTTP_PORT;
-    const TCHAR *pcwszServerName = L"localhost";
-    const WCHAR *pcwszPath = L"/WinHttpWebSocketSample/EchoWebSocket.ashx";
-    const WCHAR *pcwszMessage = L"Hello world";
-    const DWORD cdwMessageLength = ARRAYSIZE(L"Hello world") * sizeof(WCHAR);
+	DWORD dwError = ERROR_SUCCESS;
+	BOOL fStatus = FALSE;
+	HINTERNET hRequestHandle = NULL;
+	HINTERNET hWebSocketHandle = NULL;
 
-    //
-    // Create session, connection and request handles.
-    //
 
-    hSessionHandle = WinHttpOpen(L"WebSocket sample",
-                                 WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
-                                 NULL,
-                                 NULL,
-                                 0);
-    if (hSessionHandle == NULL)
-    {
-        dwError = GetLastError();
-        goto quit;
-    }
+	//
+	// Create session, connection and request handles.
+	//
 
-    hConnectionHandle = WinHttpConnect(hSessionHandle,
-                                       pcwszServerName,
-                                       Port,
-                                       0);
-    if (hConnectionHandle == NULL)
-    {
-        dwError = GetLastError();
-        goto quit;
-    }
+	m_session = WinHttpOpen(
+		m_userAgent,
+		WINHTTP_ACCESS_TYPE_AUTOMATIC_PROXY,
+		NULL,
+		NULL,
+		0);
+	if (m_session == NULL)
+	{
+		dwError = GetLastError();
+		goto quit;
+	}
 
-    hRequestHandle = WinHttpOpenRequest(hConnectionHandle,
-                                        L"GET",
-                                        pcwszPath,
-                                        NULL,
-                                        NULL,
-                                        NULL,
-                                        0);
-    if (hRequestHandle == NULL)
-    {
-        dwError = GetLastError();
-        goto quit;
-    }
+	m_connection = WinHttpConnect(m_session,
+		server,
+		port,
+		0);
+	if (m_connection == NULL)
+	{
+		dwError = GetLastError();
+		goto quit;
+	}
 
-    //
-    // Request protocol upgrade from http to websocket.
-    //
+	hRequestHandle = WinHttpOpenRequest(m_connection,
+		L"GET",
+		m_path,
+		NULL,
+		NULL,
+		NULL,
+		0);
+	if (hRequestHandle == NULL)
+	{
+		dwError = GetLastError();
+		goto quit;
+	}
+
+	//
+	// Request protocol upgrade from http to websocket.
+	//
 #pragma prefast(suppress:6387, "WINHTTP_OPTION_UPGRADE_TO_WEB_SOCKET does not take any arguments.")
-    fStatus = WinHttpSetOption(hRequestHandle,
-                               WINHTTP_OPTION_UPGRADE_TO_WEB_SOCKET,
-                               NULL,
-                               0);
-    if (!fStatus)
-    {
-        dwError = GetLastError();
-        goto quit;
-    }
+	fStatus = WinHttpSetOption(hRequestHandle,
+		WINHTTP_OPTION_UPGRADE_TO_WEB_SOCKET,
+		NULL,
+		0);
+	if (!fStatus)
+	{
+		dwError = GetLastError();
+		goto quit;
+	}
 
-    //
-    // Perform websocket handshake by sending a request and receiving server's response.
-    // Application may specify additional headers if needed.
-    //
+	//
+	// Perform websocket handshake by sending a request and receiving server's response.
+	// Application may specify additional headers if needed.
+	//
 
-    fStatus = WinHttpSendRequest(hRequestHandle,
-                                 WINHTTP_NO_ADDITIONAL_HEADERS,
-                                 0,
-                                 NULL,
-                                 0,
-                                 0,
-                                 0);
-    if (!fStatus)
-    {
-        dwError = GetLastError();
-        goto quit;
-    }
+	fStatus = WinHttpSendRequest(hRequestHandle,
+		WINHTTP_NO_ADDITIONAL_HEADERS,
+		0,
+		NULL,
+		0,
+		0,
+		0);
+	if (!fStatus)
+	{
+		dwError = GetLastError();
+		goto quit;
+	}
 
-    fStatus = WinHttpReceiveResponse(hRequestHandle, 0);
-    if (!fStatus)
-    {
-        dwError = GetLastError();
-        goto quit;
-    }
+	fStatus = WinHttpReceiveResponse(hRequestHandle, 0);
+	if (!fStatus)
+	{
+		dwError = GetLastError();
+		goto quit;
+	}
 
-    //
-    // Application should check what is the HTTP status code returned by the server and behave accordingly.
-    // WinHttpWebSocketCompleteUpgrade will fail if the HTTP status code is different than 101.
-    //
+	//
+	// Application should check what is the HTTP status code returned by the server and behave accordingly.
+	// WinHttpWebSocketCompleteUpgrade will fail if the HTTP status code is different than 101.
+	//
 
-    hWebSocketHandle = WinHttpWebSocketCompleteUpgrade(hRequestHandle, NULL);
-    if (hWebSocketHandle == NULL)
-    {
-        dwError = GetLastError();
-        goto quit;
-    }
+	m_webSocket = WinHttpWebSocketCompleteUpgrade(hRequestHandle, NULL);
+	if (hWebSocketHandle == NULL)
+	{
+		dwError = GetLastError();
+		goto quit;
+	}
 
-    //
-    // The request handle is not needed anymore. From now on we will use the websocket handle.
-    //
+	//
+	// The request handle is not needed anymore. From now on we will use the websocket handle.
+	//
 
-    WinHttpCloseHandle(hRequestHandle);
-    hRequestHandle = NULL;
+	WinHttpCloseHandle(hRequestHandle);
+	hRequestHandle = NULL;
 
-    wprintf(L"Succesfully upgraded to websocket protocol\n");
+	wprintf(L"Succesfully upgraded to websocket protocol\n");
+
+	return 0;
+quit:
+	if (hRequestHandle != NULL)
+	{
+		WinHttpCloseHandle(hRequestHandle);
+		hRequestHandle = NULL;
+	}
+	Close();
+	return -1;
+}
 
     //
     // Send and receive data on the websocket protocol.
     //
+int WinHttpWebSocket::Send(WINHTTP_WEB_SOCKET_BUFFER_TYPE buf_type, void *buf, size_t size) {
+	DWORD dwError = NO_ERROR;
+	dwError = WinHttpWebSocketSend(m_webSocket,
+		buf_type,
+		buf,
+		size);
+	if (dwError != NO_ERROR)
+	{
+		return -1;
+	}
+	return 0;
+}
 
-    dwError = WinHttpWebSocketSend(hWebSocketHandle,
-	    WINHTTP_WEB_SOCKET_UTF8_MESSAGE_BUFFER_TYPE,
-                                   (PVOID)pcwszMessage,
-                                   cdwMessageLength);
-    if (dwError != ERROR_SUCCESS)
+    //wprintf(L"Sent message to the server: '%s'\n", pcwszMessage);
+
+int WinHttpWebSocket::Receive(void *buf, size_t size, size_t *received_size, WINHTTP_WEB_SOCKET_BUFFER_TYPE *buf_type) {
+	DWORD dwError = NO_ERROR;
+
+	dwError = WinHttpWebSocketReceive(m_webSocket,
+		buf,
+		size,
+		(DWORD *)received_size,
+		buf_type);
+	if (dwError != ERROR_SUCCESS)
+	{
+		return -1;
+	}
+
+	//wprintf(L"Received message from the server: '%.*s'\n", dwBufferLength, (WCHAR*)rgbBuffer);
+	return 0;
+}
+
+
+void WinHttpWebSocket::Close(){
+	DWORD dwError = NO_ERROR;
+
+	if (m_webSocket != NULL)
+	{
+		//
+		// Gracefully close the connection.
+		//
+
+		dwError = WinHttpWebSocketClose(m_webSocket,
+			WINHTTP_WEB_SOCKET_SUCCESS_CLOSE_STATUS,
+			NULL,
+			0);
+		if (dwError != ERROR_SUCCESS)
+		{
+			printf("WinHttpWebSocketClose: %d", dwError);
+		}
+		//
+		// Check close status returned by the server.
+		//
+
+		USHORT usStatus = 0;
+		BYTE rgbCloseReasonBuffer[123];
+		DWORD dwCloseReasonLength = 0;
+		dwError = WinHttpWebSocketQueryCloseStatus(m_webSocket,
+			&usStatus,
+			rgbCloseReasonBuffer,
+			ARRAYSIZE(rgbCloseReasonBuffer),
+			&dwCloseReasonLength);
+		if (dwError != ERROR_SUCCESS)
+		{
+			printf("WinHttpWebSocketQueryCloseStatus: %d", dwError);
+		}
+
+		wprintf(L"The server closed the connection with status code: '%d' and reason: '%.*S'\n",
+			(int)usStatus,
+			dwCloseReasonLength,
+			rgbCloseReasonBuffer);
+
+		WinHttpCloseHandle(m_webSocket);
+		m_webSocket = NULL;
+	}
+
+    if (m_connection != NULL)
     {
-        goto quit;
+	    WinHttpCloseHandle(m_connection);
+	    m_connection = NULL;
     }
 
-    wprintf(L"Sent message to the server: '%s'\n", pcwszMessage);
-
-    do
+    if (m_session != NULL)
     {
-        if (dwBufferLength == 0)
-        {
-            dwError = ERROR_NOT_ENOUGH_MEMORY;
-            goto quit;
-        }
-
-        dwError = WinHttpWebSocketReceive(hWebSocketHandle,
-                                          pbCurrentBufferPointer,
-                                          dwBufferLength,
-                                          &dwBytesTransferred,
-                                          &eBufferType);
-        if (dwError != ERROR_SUCCESS)
-        {
-            goto quit;
-        }
-
-        //
-        // If we receive just part of the message restart the receive operation.
-        //
-
-        pbCurrentBufferPointer += dwBytesTransferred;
-        dwBufferLength -= dwBytesTransferred;
+	    WinHttpCloseHandle(m_session);
+	    m_session = NULL;
     }
-    while (eBufferType == WINHTTP_WEB_SOCKET_BINARY_FRAGMENT_BUFFER_TYPE);
-
-    //
-    // We expected server just to echo single binary message.
-    //
-
-    if (eBufferType != WINHTTP_WEB_SOCKET_BINARY_MESSAGE_BUFFER_TYPE)
-    {
-        wprintf(L"Unexpected buffer type\n");
-        dwError = ERROR_INVALID_PARAMETER;
-        goto quit;
-    }
-
-    wprintf(L"Received message from the server: '%.*s'\n", dwBufferLength, (WCHAR*)rgbBuffer);
-
-    //
-    // Gracefully close the connection.
-    //
-
-    dwError = WinHttpWebSocketClose(hWebSocketHandle,
-                                    WINHTTP_WEB_SOCKET_SUCCESS_CLOSE_STATUS,
-                                    NULL,
-                                    0);
-    if (dwError != ERROR_SUCCESS)
-    {
-        goto quit;
-    }
-
-    //
-    // Check close status returned by the server.
-    //
-
-    dwError = WinHttpWebSocketQueryCloseStatus(hWebSocketHandle,
-                                               &usStatus,
-                                               rgbCloseReasonBuffer,
-                                               ARRAYSIZE(rgbCloseReasonBuffer),
-                                               &dwCloseReasonLength);
-    if (dwError != ERROR_SUCCESS)
-    {
-        goto quit;
-    }
-
-    wprintf(L"The server closed the connection with status code: '%d' and reason: '%.*S'\n",
-            (int)usStatus,
-            dwCloseReasonLength,
-            rgbCloseReasonBuffer);
-
-quit:
-
-    if (hRequestHandle != NULL)
-    {
-        WinHttpCloseHandle(hRequestHandle);
-        hRequestHandle = NULL;
-    }
-
-    if (hWebSocketHandle != NULL)
-    {
-        WinHttpCloseHandle(hWebSocketHandle);
-        hWebSocketHandle = NULL;
-    }
-
-    if (hConnectionHandle != NULL)
-    {
-        WinHttpCloseHandle(hConnectionHandle);
-        hConnectionHandle = NULL;
-    }
-
-    if (hSessionHandle != NULL)
-    {
-        WinHttpCloseHandle(hSessionHandle);
-        hSessionHandle = NULL;
-    }
-
-    if (dwError != ERROR_SUCCESS)
-    {
-        wprintf(L"Application failed with error: %u\n", dwError);
-        return -1;
-    }
-
-    return 0;
 }
---
+++ libSDK/App/RawDataCapture/WinHttpWebSocket.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <Windows.h>
+#include <WinHttp.h>
+
+class WinHttpWebSocket {
+public:
+	WinHttpWebSocket() : m_session(NULL), m_connection(NULL), m_webSocket(NULL) {}
+	int Open(const WCHAR *server, INTERNET_PORT port);
+	void Close();
+	int Send(WINHTTP_WEB_SOCKET_BUFFER_TYPE buf_type, void *buf, size_t size);
+	int Receive(void *buf, size_t size, size_t *received_size, WINHTTP_WEB_SOCKET_BUFFER_TYPE *buf_type);
+private:
+	HINTERNET	m_session;
+	HINTERNET	m_connection;
+	HINTERNET	m_webSocket;
+	static const WCHAR * const m_userAgent;
+	static const WCHAR * const m_path;
+};
